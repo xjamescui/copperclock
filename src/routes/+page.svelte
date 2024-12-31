93c34ec906754c56767461f208bbfc0a41f0a5bf
James Cui <james.q.cui@gmail.com>
2024-12-30 16:06:44 -0800

    init commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<div class="flex flex-col items-center">
+	<h1>It is Copper-o-Clock</h1>
+	<div class="flex flex-col items-center">
+		<div>Last peed at</div>
+		<div>Last pooped at</div>
+	</div>
+</div>
